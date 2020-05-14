d7edd48922a9eccc16a9de7f9f1a67e62ca47146
louwb <louwb@yunrong.cn>
2020-05-14 23:22:05 +0800

    fix:新增dependecy-injection工程

---
+++ spring-dependency-injection-study/src/main/java/com/david/study/spring/dependency/injection/AnnotationDependencyInjectionDemo.java
@@ -0,0 +1,10 @@
+package com.david.study.spring.dependency.injection;
+
+/**
+ * @version $Id: null.java, v 1.0 2020/5/14 11:20 PM david Exp $$
+ * @Author:louwenbin(louwb@runyong.cn)
+ * @Description:${TODO}(这里用一句话描述这个类的作用)
+ * @since 1.0
+ **/
+public class AnnotationDependencyInjectionDemo {
+}
