79600d787e7238c5d6420fc89af2988d41a5cd04
louwb <louwb@yunrong.cn>
2020-08-10 17:13:46 +0800

    feature:Conitional注解的使用节点

--- spring-annotation/src/main/java/com/david/study/spring/conditional/ConditionalDemo.java
+++ spring-annotation/src/main/java/com/david/study/spring/conditional/ConditionalDemo.java
@@ -11,6 +11,12 @@ import org.springframework.core.env.ConfigurableEnvironment;
  * @version $Id: null.java, v 1.0 2020/8/10 3:56 PM david Exp $$
  * @Author:louwenbin(louwb@runyong.cn)
  * @Description:@Conditional 注解的使用demo
+ * @see Conditional
+ * Conditional的使用要结合Condition接口，实现matches方法
+ * 在 ConfigurationClassPostProcessor里面，先处理了@Import再处理@ImportSource,第三步处理@ComponentScan
+ * 之后 在处理完引入的注解之后，再调用ConfigurationClassBeanDefinitionReader.loadBeanDefinitions(configClass);
+ * 方法内部处理@Import和@Bean注解
+ * loanBeanDefinitionsForBeanMethod方法 会调用当前的shouldSkip方法，查找Conditional注解的Condition接口
  * @since 1.0
  **/
 @Configuration
